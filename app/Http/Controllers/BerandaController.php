6bf120f3ce70798518dccca144d804b1ba1de586
khoirul mustofa <datatoblenk@gmail.com>
2022-10-24 02:20:30 +0700

    update

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -17,24 +17,12 @@ class BerandaController extends Controller
      */
     public function index()
     {
-        if (Berita::all()->count() > 3) {
-            $response = view('beranda', [
-                "title" => "beranda",
-                "headLine" => Berita::latest()->take(3)->get(),
-                "pengurus" => Pengurus::first()->take(3)->get(),
-                "sambutan" => Sambutan::all(),
-                "banner" => Banner::all()
-            ]);
-        } else {
-            $response = view('beranda', [
-                "title" => "beranda",
-                "headLine" => Berita::all(),
-                "pengurus" => Pengurus::all(),
-                "sambutan" => Sambutan::all(),
-                "banner" => Banner::all()
-            ]);
-        }
-
-        return $response;
+        return view('beranda', [
+            "title" => "beranda",
+            "headLine" => Berita::all(),
+            "pengurus" => Pengurus::all(),
+            "sambutan" => Sambutan::all(),
+            "banner" => Banner::all()
+        ]);
     }
 }
